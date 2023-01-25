3d1b47ec431341c3a2228e278ee398ae817b77e2
nicolas_stassun <nicolas_stassun@estudante.sesisenai.org.br>
2023-01-25 16:54:58 -0300

    3.0

---
+++ src/atv3.java
@@ -0,0 +1,126 @@
+import java.util.Scanner;
+
+public class atv3 {
+
+    static Scanner sc = new Scanner(System.in);
+    static Integer[] QuantidadeProdutos = new Integer[5];
+
+    public static void main(String[] args) {
+
+        String[] NumeroProduto = new String[5];
+        String[] DescricaoProduto = new String[5];
+        Double[] PreçoProduto = new Double[5];
+
+        NumeroProduto[0] = "01";
+        DescricaoProduto[0] = "HD 1Tb";
+        PreçoProduto[0] = 500.0;
+
+        NumeroProduto[1] = "02";
+        DescricaoProduto[1] = "SSD 128gb";
+        PreçoProduto[1] = 360.0;
+
+        NumeroProduto[2] = "03";
+        DescricaoProduto[2] = "Placa Mãe Ag563";
+        PreçoProduto[2] = 867.50;
+
+        NumeroProduto[3] = "04";
+        DescricaoProduto[3] = "Processador i5 9900K";
+        PreçoProduto[3] = 1230.75;
+
+        NumeroProduto[4] = "05";
+        DescricaoProduto[4] = "Placa de Vídeo RTX 2060";
+        PreçoProduto[4] = 2154.0;
+
+        menu(NumeroProduto,DescricaoProduto,PreçoProduto);
+
+
+    }
+    public static void menu(String[] NumeroProduto, String[] DescricaoProduto, Double[] PreçoProduto){
+
+        for (int i =0; i!=3;i=i) {
+            System.out.println("---------Loja CTW---------\n" +
+                    "1- Mostrar produtos\n" +
+                    "2- Adicionar Item\n" +
+                    "3- Finalizar Compra\n"+
+                    "----------------------------\n");
+            i = sc.nextInt();
+
+            switch (i){
+                case 1:
+
+                    MostraProdutos(NumeroProduto,DescricaoProduto,PreçoProduto);
+
+                    break;
+
+                case 2:
+
+                    AdicionarItem(NumeroProduto,DescricaoProduto,PreçoProduto);
+
+                    break;
+
+                case 3:
+
+                    MostraProdutos(NumeroProduto,DescricaoProduto,PreçoProduto);
+
+                    break;
+
+            }
+        }
+    }
+
+    public static void MostraProdutos(String[] NumeroProduto, String[] DescricaoProduto, Double[] PreçoProduto){
+
+        for(int i=0;i<=4;i++){
+
+            System.out.println(NumeroProduto[i]+" - "+DescricaoProduto[i]+" R$"+PreçoProduto[i]);
+
+        }
+        menu(NumeroProduto,DescricaoProduto,PreçoProduto);
+    }
+
+    public static void AdicionarItem(String[] NumeroProduto, String[] DescricaoProduto, Double[] PreçoProduto){
+        String AdcNum ="";
+        int quantidade = 0;
+        System.out.println("Informe o código do item que você deseja adicionar ao seu carrinho:");
+        AdcNum = sc.next();
+        sc.nextLine();
+        for (int i=0;i<=4;i++) {
+
+            if (NumeroProduto[i] == AdcNum) {
+
+                System.out.println("Informe a quantidade que você deseja adicionar ao seu carrinho: ");
+                quantidade = sc.nextInt();
+                QuantidadeProdutos[i] = quantidade;
+
+            }
+        }
+        menu(NumeroProduto,DescricaoProduto,PreçoProduto);
+
+    }
+    public static void FinalizarCompra(String[] NumeroProduto, String[] DescricaoProduto, Double[] PreçoProduto){
+
+        double totalCompra=0.0;
+        for (int i=0;i<=4;i++) {
+            if (QuantidadeProdutos[i]!=0) {
+
+                if(i==0){
+                    System.out.println(QuantidadeProdutos[0]+" | "+NumeroProduto[0]+" - "+DescricaoProduto[0]+" R$"+PreçoProduto[0]);
+                }
+                else if(i==1){
+                    System.out.println(QuantidadeProdutos[1]+" | "+NumeroProduto[1]+" - "+DescricaoProduto[1]+" R$"+PreçoProduto[1]);
+                }
+                else if(i==2){
+                    System.out.println(QuantidadeProdutos[2]+" | "+NumeroProduto[2]+" - "+DescricaoProduto[2]+" R$"+PreçoProduto[2]);
+                }
+                else if(i==3){
+                    System.out.println(QuantidadeProdutos[3]+" | "+NumeroProduto[3]+" - "+DescricaoProduto[3]+" R$"+PreçoProduto[3]);
+                }
+                else if(i==4){
+                    System.out.println(QuantidadeProdutos[4]+" | "+NumeroProduto[4]+" - "+DescricaoProduto[4]+" R$"+PreçoProduto[4]);
+                }
+
+            }
+        }
+    }
+
+}
---
+++ src/atv4.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class atv4 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Double NotaAluno =0.0;
+        char NotaAlunoChar = 'z';
+
+        System.out.println("Informe a Nota do Aluno: ");
+        NotaAluno = sc.nextDouble();
+
+       classificarNota(NotaAluno,NotaAlunoChar);
+
+        System.out.println(NotaAlunoChar);
+    }
+
+    public static char classificarNota(double NotaAluno, char NotaAlunoChar){
+
+        if (NotaAluno >= 9.0){
+
+            NotaAlunoChar = 'A';
+
+
+        }
+        else if (NotaAluno >= 8.0){
+
+            NotaAlunoChar = 'B';
+
+        }
+        else if (NotaAluno >= 7.0){
+
+            NotaAlunoChar = 'C';
+
+        }
+        else if (NotaAluno >= 6.0){
+
+            NotaAlunoChar = 'D';
+
+        }
+        else if (NotaAluno < 6.0){
+
+            NotaAlunoChar = 'E';
+
+        }
+        return NotaAlunoChar;
+    }
+
+}
